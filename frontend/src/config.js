4dd5eaf9b86971199340c443b9d84fc2f2aee7f5
akosklema <klema.akos@gmail.com>
2022-11-09 12:31:43 +0100

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const configData = {
-  SERVER_URL: 'http://localhost:8080',
-  // SERVER_URL: 'http://host.docker.internal:8080',
+  // SERVER_URL: 'http://localhost:8080',
+  SERVER_URL: 'https://product-feedback-api.onrender.com',
 };
 
 export default configData;
